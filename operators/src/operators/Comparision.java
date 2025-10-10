9b33a619f821f30c6a51e6bd584fd7b9c76b8db2
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-10 23:14:00 +0530

    Update Comparision.java

--- operators/src/operators/Comparision.java
+++ operators/src/operators/Comparision.java
@@ -10,10 +10,8 @@ public class Comparision {
 		System.out.println("a<=b :"+(a<=b));
 		System.out.println("a>=b  :"+(a>=b));
 		System.out.println("a<b :"+(a<b));
-		System.out.println("a>b  :"+(a>b));
-		
-		
+		System.out.println("a>b  :"+(a>b));	
 	}
-
 }
+
 
