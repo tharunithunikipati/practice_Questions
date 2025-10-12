39a0162c6433a100e833556f0ec669cd1e0838a0
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-12 11:41:00 +0530

    Update oddnumbers.java

--- operators/src/operators/oddnumbers.java
+++ operators/src/operators/oddnumbers.java
@@ -3,11 +3,9 @@ public class oddnumbers {
 	public static void main(String args[]) {
 		int i=1;
 		for(; i<=20; i++) {
-			System.out.println(i+2);
-			
-		}
-		
+			System.out.println(i+2);	
+		}	
 	}
-
 }
+
 
