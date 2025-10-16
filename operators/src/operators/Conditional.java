7dbd296e67647e5edb907399aac3ed40a683822f
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-16 15:49:00 +0530

    Reformat Conditional.java with consistent package import

--- operators/src/operators/Conditional.java
+++ operators/src/operators/Conditional.java
@@ -1,4 +1,5 @@
 package operators;
+import java.util.*;
 public class Conditional {
 	public static void main(String args[]) {
 		int a=12;
@@ -7,6 +8,7 @@ public class Conditional {
 		}
 	}
 }
+
 
 
 
