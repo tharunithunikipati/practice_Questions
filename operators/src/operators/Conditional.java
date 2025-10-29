6a56ab3621cb8ff75ecdf91b82bffc24e03925f0
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-10-29 12:01:20 +0530

    Update Conditional.java

--- operators/src/operators/Conditional.java
+++ operators/src/operators/Conditional.java
@@ -4,11 +4,11 @@ public class Conditional {
 	public static void main(String args[]) {
 		int a=12;
 		if(a==12) {
-			System.out.println("A value:"+a);
-			
+			System.out.println("A value:"+a);	
 		}	
 	}
 }
+
 
 
 
