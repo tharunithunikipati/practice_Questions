3c11e9f7967e49960af86659c1a2057f45b336a8
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-11-02 15:36:14 +0530

    Update Test1.java

--- operators/src/operators/Test1.java
+++ operators/src/operators/Test1.java
@@ -7,6 +7,7 @@ public class Test1 {
 	        Test1 t1 = new Test1(); 
 	        Test1 t2 = new Test1(); 
 	        t1.x = 20; 
+			
 	        System.out.print(t1.x + " "); 
 	        System.out.println(t2.x); 
 	    } 
