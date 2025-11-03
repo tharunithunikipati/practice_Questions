1626c100a3bf71c934c9f3b2ddea8ea0067524ff
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-11-03 14:41:46 +0530

    Fix formatting issues in Sum.java

--- operators/src/operators/Sum.java
+++ operators/src/operators/Sum.java
@@ -1,6 +1,5 @@
 package operators;
 import java.util.*;
-
 public class Sum {
 	public static void main(String args[]) {
 		int sum=0;
@@ -10,6 +9,7 @@ public class Sum {
 		System.out.println(sum);
 	}
 }	
+
 
 
 
