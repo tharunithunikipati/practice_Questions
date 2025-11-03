c9776b3654ab883927772533db1c538998cade41
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-11-03 14:40:28 +0530

    Update Loops.java

--- operators/src/operators/Loops.java
+++ operators/src/operators/Loops.java
@@ -3,10 +3,12 @@ import java.util*;
 public class Loops {
 	public static void main(String args[]) {
 		for (int i=1; i<=5; i++) {
-		    System.out.println(i*i);		
+		    System.out.println(i*i);
+			
 		}
 	}
 }
+
 
 
 
