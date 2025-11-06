f7ffdfef94a623a22b616d8493d93ac83030c466
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-11-06 09:26:45 +0530

    Format Assignment.java with consistent spacing

--- operators/src/operators/Assignment.java
+++ operators/src/operators/Assignment.java
@@ -1,6 +1,7 @@
 package operators;
 import java.util.*;
 
+
 public class Assignment {
 	public static void main(String args[]) {
 		int a=2;
@@ -17,6 +18,7 @@ public class Assignment {
 		System.out.println("a%=6 :"+a);
 	}
 }
+
 
 
 
