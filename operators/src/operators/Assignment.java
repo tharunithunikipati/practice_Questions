f89720fa182bc8e9398d80d5f9112254625e8496
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-11-17 21:10:02 +0530

    Update Assignment.java

--- operators/src/operators/Assignment.java
+++ operators/src/operators/Assignment.java
@@ -1,5 +1,17 @@
 package operators;
 import java.util;
+
+
+
+
+
+
+
+
+
+
+
+
 class Assignment {
 	public static void main(String args[]) {
 		int a=2;
@@ -16,6 +28,7 @@ class Assignment {
 		System.out.println("a%=6 :"+a);
 	}
 }
+
 
 
 
