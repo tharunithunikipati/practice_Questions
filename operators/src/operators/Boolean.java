7a43d27f8a4c0b460392d0a97389c26f11f4a5ec
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-12-01 20:59:42 +0530

    Update Boolean.java

--- operators/src/operators/Boolean.java
+++ operators/src/operators/Boolean.java
@@ -4,8 +4,7 @@ public class Boolean {
 
 
 
-
-	public static void main(String args[]) {
+public static void main(String args[]) {
 		int a=10;
 		int b=2;
 		System.out.println("a=  :"+a);
@@ -16,6 +15,7 @@ public class Boolean {
 		System.out.println("a <<b :"+(a>>b));
 	}
 }
+
 
 
 
