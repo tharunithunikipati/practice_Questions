a831d6a7abe8f90a2de14a6e68a0dc3ef54bf927
ThunikipatiTharuni <164206827+tharunithunikipati@users.noreply.github.com>
2025-12-27 10:30:58 +0530

    Update Comparision.java

--- operators/src/operators/Comparision.java
+++ operators/src/operators/Comparision.java
@@ -1,6 +1,7 @@
 package operators;
 import java.util*;
 public class Comparision {
+	
 	public static void main(String args[]) {
 		int a=9;
 		int b=8;
@@ -14,6 +15,7 @@ public class Comparision {
 		System.out.println("a>b  :"+(a>b));			
 	}
 }
+
 
 
 
